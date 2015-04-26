1255aa563a65712a88db7edfe0b8da0c6f616f48
natsumiaya <natsumiaya@users.noreply.github.com>
2015-04-26 15:50:24 +0700

    add login and register page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
-
+var mongoose = require('mongoose');
+var User = require('../models/user')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  /*example mongoose
+  var user = User({
+  firstname : 'test',
+  lastname  : 'test',
+  email     : 'test',
+  username  : 'test',
+  password  : 'test'
+	});
+  user.save();*/
 });
+
+/* GET log in page */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+  /*example mongoose
+  var user = User({
+  firstname : 'test',
+  lastname  : 'test',
+  email     : 'test',
+  username  : 'test',
+  password  : 'test'
+  });
+  user.save();*/
+});
+
+/* GET register page */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Express' });
+  /*example mongoose
+  var user = User({
+  firstname : 'test',
+  lastname  : 'test',
+  email     : 'test',
+  username  : 'test',
+  password  : 'test'
+  });
+  user.save();*/
+});
+
 
 module.exports = router;
