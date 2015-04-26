bb573ad0927df525891b56f0cd318e0e966d4258
Karsten Ari Agathon <karstenaa@gmail.com>
2015-04-26 15:10:41 +0700

    add model user

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+var mongoose = require('mongoose');
+var User = require('../models/user')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  /*example mongoose
+  var user = User({
+  firstname : 'test',
+  lastname  : 'test',
+  email     : 'test',
+  username  : 'test',
+  password  : 'test'
+	});
+  user.save();*/
 });
+
+
 
 module.exports = router;
