46b98e4c5fa7a62602e7d75f11a27dea7b3333cb
Iskandar <handyboy.ikhank@gmail.com>
2015-04-28 21:09:37 +0700

    Added authentication using passport.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,59 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+
+var session = require('express-session');
+var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy;
+
 var User = require('../models/user');
 var Node = require('../models/node');
 var Usage = require('../models/usage');
+
+express.use(session{secret: "secretdude"});
+express.use(passport.initialize());
+express.use(passport.session());
+
+passport.use(new LocalStrategy(function(username, password, done) {
+  //fetch data here 
+  //fetch data code using mongo (rows means query result)
+  if(rows.length > 0) {
+    if(rows[0].password == password) 
+    {
+      done(null, {
+                    firstname: rows[0].firstname,
+                    lastname  : rows[0].lastname,
+                    email     : rows[0].email,
+                    username  : rows[0].username,
+                    password  : rows[0].password,
+                    nodes     : rows[0].nodes 
+                  });
+    }
+    else
+    {
+      done(null, false);
+    }
+  }
+  else
+  {
+    done(null, false);
+  }
+}));
+
+// called in number 2 argument get
+// registration using another function without passport
+var authentication = function(req, res, done) 
+{
+  if(req.isAuthenticated())
+  {
+    done();
+  }
+  else
+  {
+    // redirect to error page ex -> res.redirect('error page');
+  }
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -19,7 +69,7 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET log in page */
-router.get('/login', function(req, res, next) {
+router.get('/login', authentication ,function(req, res, next) {
   res.render('login', { title: 'Express' });
   /*example mongoose
   var user = User({
